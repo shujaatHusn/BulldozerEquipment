5752e1fe6ea51ec5cdecb89b01090c4d0f22c372
shujaatHusn <56890718+shujaatHusn@users.noreply.github.com>
2019-10-26 00:34:32 +0500

    Add files via upload

---
+++ bulldozers.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 27 03:22:50 2019
+
+@author: Husun Shujaat
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import re
+import math
+
+# Importing the dataset
+trainDf = pd.read_csv('Train.csv', low_memory=False, 
+                     parse_dates=["saledate"])
+testDf = pd.read_csv('Test.csv', low_memory=False, parse_dates=["saledate"]);
+
+dataset = pd.concat([trainDf,testDf], axis=0, sort=False);
+
+def add_datepart(df, fldname, drop=True):
+    fld = df[fldname]
+    if not np.issubdtype(fld.dtype, np.datetime64):
+        df[fldname] = fld = pd.to_datetime(fld, 
+                                     infer_datetime_format=True)
+    targ_pre = re.sub('[Dd]ate$', '', fldname)
+    for n in ('Year', 'Month', 'Week', 'Day', 'Dayofweek', 
+            'Dayofyear', 'Is_month_end', 'Is_month_start', 
+            'Is_quarter_end', 'Is_quarter_start', 'Is_year_end', 
+            'Is_year_start'):
+        df[targ_pre+n] = getattr(fld.dt,n.lower())
+    df[targ_pre+'Elapsed'] = fld.astype(np.int64) // 10**9
+    if drop: df.drop(fldname, axis=1, inplace=True)
+    
+    
+add_datepart(dataset,'saledate')
+#X.saleIs_year_end.head()
+
+def train_cats(df):
+    """Change any columns of strings in a panda's dataframe to a column of
+    categorical values. This applies the changes inplace.
+    Parameters:
+    -----------
+    df: A pandas dataframe. Any columns of strings will be changed to
+        categorical values.
+    Examples:
+    ---------
+    >>> df = pd.DataFrame({'col1' : [1, 2, 3], 'col2' : ['a', 'b', 'a']})
+    >>> df
+       col1 col2
+    0     1    a
+    1     2    b
+    2     3    a
+    note the type of col2 is string
+    >>> train_cats(df)
+    >>> df
+       col1 col2
+    0     1    a
+    1     2    b
+    2     3    a
+    now the type of col2 is category
+    """
+    for n,c in df.items():
+        if pd.api.types.is_string_dtype(c): df[n] = c.astype('category').cat.as_ordered()
+
+def apply_cats(df, trn):
+    """Changes any columns of strings in df into categorical variables using trn as
+    a template for the category codes.
+    Parameters:
+    -----------
+    df: A pandas dataframe. Any columns of strings will be changed to
+        categorical values. The category codes are determined by trn.
+    trn: A pandas dataframe. When creating a category for df, it looks up the
+        what the category's code were in trn and makes those the category codes
+        for df.
+    Examples:
+    ---------
+    >>> df = pd.DataFrame({'col1' : [1, 2, 3], 'col2' : ['a', 'b', 'a']})
+    >>> df
+       col1 col2
+    0     1    a
+    1     2    b
+    2     3    a
+    note the type of col2 is string
+    >>> train_cats(df)
+    >>> df
+       col1 col2
+    0     1    a
+    1     2    b
+    2     3    a
+    now the type of col2 is category {a : 1, b : 2}
+    >>> df2 = pd.DataFrame({'col1' : [1, 2, 3], 'col2' : ['b', 'a', 'a']})
+    >>> apply_cats(df2, df)
+           col1 col2
+        0     1    b
+        1     2    a
+        2     3    a
+    now the type of col is category {a : 1, b : 2}
+    """
+    for n,c in df.items():
+        if (n in trn.columns) and (trn[n].dtype.name=='category'):
+            df[n] = c.astype('category').cat.as_ordered()
+            df[n].cat.set_categories(trn[n].cat.categories, ordered=True, inplace=True)
+            
+train_cats(dataset)
+dataset.UsageBand.cat.set_categories(['High', 'Medium', 'Low'],
+    ordered=True, inplace=True)
+
+def proc_df(df, y_fld=None, skip_flds=None, ignore_flds=None, do_scale=False, na_dict=None,
+            preproc_fn=None, max_n_cat=None, subset=None, mapper=None):
+
+    if not ignore_flds: ignore_flds=[]
+    if not skip_flds: skip_flds=[]
+    if subset: df = get_sample(df,subset)
+    else: df = df.copy()
+    ignored_flds = df.loc[:, ignore_flds]
+    df.drop(ignore_flds, axis=1, inplace=True)
+    if preproc_fn: preproc_fn(df)
+    if y_fld is None: y = None
+    else:
+        if not pd.api.types.is_numeric_dtype(df[y_fld]): df[y_fld] = pd.Categorical(df[y_fld]).codes
+        y = df[y_fld].values
+        skip_flds += [y_fld]
+    df.drop(skip_flds, axis=1, inplace=True)
+
+    if na_dict is None: na_dict = {}
+    else: na_dict = na_dict.copy()
+    na_dict_initial = na_dict.copy()
+    for n,c in df.items(): na_dict = fix_missing(df, c, n, na_dict)
+    if len(na_dict_initial.keys()) > 0:
+        df.drop([a + '_na' for a in list(set(na_dict.keys()) - set(na_dict_initial.keys()))], axis=1, inplace=True)
+    if do_scale: mapper = scale_vars(df, mapper)
+    for n,c in df.items(): numericalize(df, c, n, max_n_cat)
+    df = pd.get_dummies(df, dummy_na=True)
+    df = pd.concat([ignored_flds, df], axis=1)
+    res = [df, y, na_dict]
+    if do_scale: res = res + [mapper]
+    return res
+
+def numericalize(df, col, name, max_n_cat):
+    if not pd.api.types.is_numeric_dtype(col) and ( max_n_cat is None or len(col.cat.categories)>max_n_cat):
+        df[name] = pd.Categorical(col).codes+1
+
+def fix_missing(df, col, name, na_dict):
+    if pd.api.types.is_numeric_dtype(col):
+        if pd.isnull(col).sum() or (name in na_dict):
+            df[name+'_na'] = pd.isnull(col)
+            filler = na_dict[name] if name in na_dict else col.median()
+            df[name] = col.fillna(filler)
+            na_dict[name] = filler
+    return na_dict
+
+X, y, nas = proc_df(dataset, 'SalePrice')
+
+#split dataset into validation and train set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 12457)
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size = 80225)
+
+def rmse(x,y): return math.sqrt(((x-y)**2).mean())
+
+def print_score(m):
+    res = [rmse(m.predict(X_train), y_train), rmse(m.predict(X_valid), y_valid),
+                m.score(X_train, y_train), m.score(X_valid, y_valid)]
+    if hasattr(m, 'oob_score_'): res.append(m.oob_score_)
+    print(res)
+    
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import forest
+m = RandomForestRegressor()
+%time m.fit(X_train, y_train)
+
+rfTestScore=m.score(X_train,y_train)
+rfValScore=m.score(X_val,y_val)
+rfOobScore=m.oob_score_
+
+
+predictions=m.predict(X_test)
+submission = pd.DataFrame({'Id':df_test['Id'],'SalePrice':predictions})
+
+filename = 'Bulldozer Eqp Prices.csv'
+
+submission.to_csv(filename,index=False)
+
+
